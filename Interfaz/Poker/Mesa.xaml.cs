94b3527eed44ae80a28998ddcf22c847af7608b5
Unknown <alessandro.fazio.perez@gmail.com>
2019-05-02 08:14:55 -0600

    Interfaz lista

--- Interfaz/Poker/Mesa.xaml.cs
+++ Interfaz/Poker/Mesa.xaml.cs
@@ -30,15 +30,17 @@ namespace Poker {
             DragMove();
         }
 
-        private void Btn_max_Click(object sender, RoutedEventArgs e) {
-            if (this.wnd_ventana.WindowState == WindowState.Normal) {
-                this.wnd_ventana.WindowState = WindowState.Maximized;
-            }
+        //private void Btn_max_Click(object sender, RoutedEventArgs e) {
+        //    if (this.wnd_ventana.WindowState == WindowState.Normal) {
+        //        this.wnd_ventana.WindowState = WindowState.Maximized;
+        //        this.Icn_max.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
+        //    }
 
-            else {
-                this.wnd_ventana.WindowState = WindowState.Normal;
-            }
-        }
+        //    else {
+        //        this.wnd_ventana.WindowState = WindowState.Normal;
+        //        this.Icn_max.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+        //    }
+        //}
 
         private void Btn_min_Click(object sender, RoutedEventArgs e) {
             this.wnd_ventana.WindowState = WindowState.Minimized;
@@ -61,30 +63,36 @@ namespace Poker {
             this.sld_apuesta.Value += 1;
         }
 
-        //private void Button_Click(object sender, RoutedEventArgs e) {
-        //    if (!this.fuera) {
-        //        this.carta1.Visibility = Visibility.Hidden;
-        //        this.carta2.Visibility = Visibility.Hidden;
-        //        this.fuera = true;
-        //    }
-        //}
+        private void Btn_fold_Click(object sender, RoutedEventArgs e) {
+            if (!this.fuera) {
+                this.Rtg_carta1_jugador3.Visibility = Visibility.Hidden;
+                this.Rtg_carta2_jugador3.Visibility = Visibility.Hidden;
+                this.fuera = true;
+            }
+        }
 
-        //private void E_jugador1_MouseEnter(object sender, MouseEventArgs e) {
-        //    if (this.fuera) {
-        //        this.carta1.Opacity = 0.60;
-        //        this.carta2.Opacity = 0.60;
-        //        this.carta1.Visibility = Visibility.Visible;
-        //        this.carta2.Visibility = Visibility.Visible;
-        //    }
-        //}
+        private void Elp_jugador3_MouseEnter(object sender, MouseEventArgs e) {
+            if (this.fuera) {
+                this.Rtg_carta1_jugador3.Opacity = 0.60;
+                this.Rtg_carta2_jugador3.Opacity = 0.60;
+                this.Rtg_carta1_jugador3.Visibility = Visibility.Visible;
+                this.Rtg_carta2_jugador3.Visibility = Visibility.Visible;
+            }
+        }
 
-        //private void E_jugador1_MouseLeave(object sender, MouseEventArgs e) {
-        //    if (this.fuera) {
-        //        this.carta1.Visibility = Visibility.Hidden;
-        //        this.carta2.Visibility = Visibility.Hidden;
-        //    }
-        //}
+        private void Elp_jugador3_MouseLeave(object sender, MouseEventArgs e) {
+            if (this.fuera) {
+                this.Rtg_carta1_jugador3.Visibility = Visibility.Hidden;
+                this.Rtg_carta2_jugador3.Visibility = Visibility.Hidden;
+            }
+        }
 
+        private void Sp_jugador3_MouseEnter(object sender, MouseEventArgs e) {
+            this.Elp_jugador3_MouseEnter(sender, e);
+        }
 
+        private void Sp_jugador3_MouseLeave(object sender, MouseEventArgs e) {
+            this.Elp_jugador3_MouseLeave(sender, e);
+        }
     }
 }
