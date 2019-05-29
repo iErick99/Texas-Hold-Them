2f8ffcba558a7acad4b9a514c522c2ac4c96ee04
Erick <lolerick1@hotmail.com>
2019-05-29 22:33:01 +0000

    Intercambio de informacion entre el servidor y el cliente

--- Servidor/Servidor/Program.cs
+++ Servidor/Servidor/Program.cs
@@ -6,10 +6,10 @@ namespace Servidor
     class Program {
         static void Main(string[] args) {
             // Aca puse la dir. IP loopback por mientras para que no tengan que estarla cambiando al correrlo
-            //Server server = new Server(IPAddress.Loopback.ToString(), 100);
+            Server server = new Server(IPAddress.Loopback.ToString(), 100);
 
-            //server.Start();
-            //server.Run();
+            server.Start();
+            server.Run();
 
 
             Controller controller = new Controller();
